5b7dfbf545928e2b4a5dc4b33a21c927a55b66ae
Dipankar2020-ai <63770793+Dipankar2020-ai@users.noreply.github.com>
2021-08-28 23:04:30 +0530

    Create while and for loop.py

---
+++ Python Control flow/while and for loop.py
@@ -0,0 +1,12 @@
+a=10
+while(a>0):
+    print(a)
+    a=a-1
+for i in "python":
+    print(i)
+for i in range(5):
+    print(i)
+for i in range( 2,10):
+    print(i)
+for i in range(1,11,2):
+    print(i)
