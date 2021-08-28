4f109cfe30287d0043b3b1411bf4b2e180bc4a75
Dipankar2020-ai <63770793+Dipankar2020-ai@users.noreply.github.com>
2021-08-28 21:48:33 +0530

    Add files via upload

---
+++ Python Basics/check positive or negative.py
@@ -0,0 +1,21 @@
+a=5
+if a>0:
+    print("The number is positive")
+elif a==0:
+    print("The numner is equal to zero")
+else:
+    print("the number is negative")
+a=0
+if a>0:
+    print("The number is positive")
+elif a==0:
+    print("The numner is equal to zero")
+else:
+    print("the number is negative")
+a=-9
+if a>0:
+    print("The number is positive")
+elif a==0:
+    print("The numner is equal to zero")
+else:
+    print("the number is negative")
